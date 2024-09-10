23675d305ca68c92b1d5df3ccc7a171cf88bd43a
ascpial <mail@ascpial.fr>
2024-09-10 22:16:33 +0200

    Fix code styling

--- verticalLine.lua
+++ verticalLine.lua
@@ -4,25 +4,25 @@ local expect = require "cc.expect".expect -- DO NOT COPY THIS LINE
 
 --- Draws a vertical line at a position with the specified height.
 ---@param win window The window to draw on
----@param x number The X position of the left side of the line
----@param y number The Y position of the line
----@param height number The width/length of the line
----@param side "right"|"left"|nil THe side of the line, either right or left (defaults to left)
+---@param x number The X position of the line
+---@param y number The Y position of the top of the line
+---@param height number The height of the line
+---@param right boolean|nil Wether to align the line to the right instead of the left (defaults to false)
 ---@param fgColor color|nil The color of the line (defaults to white)
 ---@param bgColor color|nil The color of the background (defaults to black)
-function PrimeUI.verticalLine(win, x, y, height, side, fgColor, bgColor)
+function PrimeUI.verticalLine(win, x, y, height, right, fgColor, bgColor)
     expect(1, win, "table")
     expect(2, x, "number")
     expect(3, y, "number")
     expect(4, height, "number")
-    side = expect(5, side, "string", "nil") or "left"
+    right = expect(5, right, "boolean", "nil") or false
     fgColor = expect(6, fgColor, "number", "nil") or colors.white
     bgColor = expect(7, bgColor, "number", "nil") or colors.black
     -- Use drawing characters to draw a thin line.
-    win.setTextColor(side=="left" and fgColor or bgColor)
-    win.setBackgroundColor(side=="left" and bgColor or fgColor)
+    win.setTextColor(right and bgColor fgColor)
+    win.setBackgroundColor(right and fgColor or bgColor)
     for j=1, height do
-      win.setCursorPos(x, y+j-1)
-      win.write(("\x95"))
+        win.setCursorPos(x, y+j-1)
+        win.write("\x95")
     end
 end
