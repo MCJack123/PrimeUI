be4d77acbd6e40c58c1feeca620ed5a66c340ef6
JackMacWindows <jackmacwindowslinux@gmail.com>
2024-09-11 17:30:31 -0400

    Update verticalLine.lua

--- verticalLine.lua
+++ verticalLine.lua
@@ -7,7 +7,7 @@ local expect = require "cc.expect".expect -- DO NOT COPY THIS LINE
 ---@param x number The X position of the line
 ---@param y number The Y position of the top of the line
 ---@param height number The height of the line
----@param right boolean|nil Wether to align the line to the right instead of the left (defaults to false)
+---@param right boolean|nil Whether to align the line to the right instead of the left (defaults to false)
 ---@param fgColor color|nil The color of the line (defaults to white)
 ---@param bgColor color|nil The color of the background (defaults to black)
 function PrimeUI.verticalLine(win, x, y, height, right, fgColor, bgColor)
@@ -19,10 +19,10 @@ function PrimeUI.verticalLine(win, x, y, height, right, fgColor, bgColor)
     fgColor = expect(6, fgColor, "number", "nil") or colors.white
     bgColor = expect(7, bgColor, "number", "nil") or colors.black
     -- Use drawing characters to draw a thin line.
-    win.setTextColor(right and bgColor fgColor)
+    win.setTextColor(right and bgColor or fgColor)
     win.setBackgroundColor(right and fgColor or bgColor)
-    for j=1, height do
-        win.setCursorPos(x, y+j-1)
+    for j = 1, height do
+        win.setCursorPos(x, y + j - 1)
         win.write("\x95")
     end
 end
